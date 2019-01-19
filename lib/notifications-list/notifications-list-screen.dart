7a2ebe09ec3098532504093e180872740767300b
Davis Araujo <davidsdearaujo@gmail.com>
2019-01-19 21:36:21 -0200

    Adicionada listagem de apenas notificações recebidas;

--- lib/notifications-list/notifications-list-screen.dart
+++ lib/notifications-list/notifications-list-screen.dart
@@ -4,6 +4,10 @@ import 'tabs/recebidas/recebidas-tab.dart';
 import '../firebase-messaging-receive/scaffold-notification-mixin.dart';
 
 class NotificationsListScreen extends StatefulWidget {
+  final bool somenteRecebidos;
+
+  const NotificationsListScreen({Key key, this.somenteRecebidos = false})
+      : super(key: key);
   @override
   _NotificationsListScreenState createState() =>
       _NotificationsListScreenState();
@@ -11,9 +15,22 @@ class NotificationsListScreen extends StatefulWidget {
 
 class _NotificationsListScreenState extends State<NotificationsListScreen>
     with ScaffoldNotificationMixin {
-
   @override
   Widget build(BuildContext context) {
+    return (widget.somenteRecebidos) ? _buildSomenteRecebidos() : _buildTabs();
+  }
+
+  Widget _buildSomenteRecebidos() {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: Text("Histórico de notificações recebidas"),
+      ),
+      body: RecebidasTab(),
+    );
+  }
+
+  Widget _buildTabs() {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
